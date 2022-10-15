4df277bc30d049e9ea585f59cd60de9e4d2c4d16
fatoumattatrawalleh <fatoumattatrawalleh64@gmail.com>
2022-10-15 03:38:38 -0700

    implementing the required

--- src/server.js
+++ src/server.js
@@ -3,12 +3,32 @@ const app = express();
 
 
 
+const connect = require('./modules/connect');
+const routes = require('./routes/router');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+
+// Connect to Database
+void (async () => {
+    try {
+        await connect();
+        console.log('connect to database');
+    }  catch (error) {
+        console.log('error connecting to database:', error.message);
+    }
+})();
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'peace'});
 });
 
-
+app.use('/api', routes);
 module.exports = app;
