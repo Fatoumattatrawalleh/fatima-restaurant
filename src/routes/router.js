a384f713649c196e8c4328cca94c33a79d8352d7
fatoumattatrawalleh <fatoumattatrawalleh64@gmail.com>
2022-11-19 04:33:25 -0800

    implementing task

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,19 +3,24 @@ const router = express.Router();
 const { createUser, getAllUsers, getOneUser, updateOne, deleteOne} =
  require('../controllers/user');
 
-const { createSale, } = require('../controllers/sale');
+const { createSale, getAllSale, getOneSale,updateOneSale,deleteOneSale} = require('../controllers/sale');
 
 // User route
 router.post('/user', createUser);
 router.get('/user', getAllUsers);
 router.get('/user/:id', getOneUser);
-router.put('/user/:id', updateOne);
+router.put('/user/:id', updateOneSale);
 router.delete('/user/:id' , deleteOne)
 
 
 
 // sale route
 router.post('/sale', createSale);
+router.get('/sale', getAllSale);
+router.get('/sale/:id', getOneSale);
+router.put('/sale/:id', updateOne);
+router.delete('/sale/:id', deleteOneSale);
+
 
 
 module.exports = router;
